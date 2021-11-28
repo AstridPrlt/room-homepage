4da1a1965fcbd2c3cd16d1ecc10d0f931264f814
AstridPrlt <31550071+AstridPrlt@users.noreply.github.com>
2021-11-28 19:46:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Frontend Mentor - Room homepage solution
+
+This is a solution to the [Room homepage challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/room-homepage-BtdBY_ENq). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [The challenge](#the-challenge)
+- [Screenshot](#screenshot)
+- [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Navigate the slider using either their mouse/trackpad or keyboard
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Javascript object-oriented
+
+
+## Author
+
+- Website - [Astrid Perillat](https://astrid-perillat.fr/)
---
+++ main.js
@@ -0,0 +1,82 @@
+class Main {
+
+    constructor() {
+        this.slider = [
+            {
+                "image": "images/desktop-image-hero-1.jpg",
+                "title": "Discover innovative ways to decorate",
+                "text": "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+                "link": 'Shop now<img src="images/icon-arrow.svg" alt="">'
+            },
+            {
+                "image": "images/desktop-image-hero-2.jpg",
+                "title": "We are available all across the globe",
+                "text": "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+                "link": 'Shop now<img src="images/icon-arrow.svg" alt="">'
+            },
+            {
+                "image": "images/desktop-image-hero-3.jpg",
+                "title": "Manufactured with the best materials",
+                "text": "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+                "link": 'Shop now<img src="images/icon-arrow.svg" alt="">'
+            }
+        ];
+
+        this.currentSlide = 0;
+        
+        this.title = document.getElementById("title");
+        this.text = document.getElementById("paragraph");
+        this.link = document.getElementById("link");
+        this.img = document.getElementById("heroImg");
+        
+        this.setSlider();
+        
+        document.getElementById("btnRight").addEventListener('click', () => {this.forward()});
+        document.getElementById("btnLeft").addEventListener("click", () => {this.backward()});
+
+        this.isMenuOpened = false;
+
+        document.getElementById('burgerButton').addEventListener("click", () => {
+            this.isMenuOpened == false ? this.showMenu() : this.hideMenu()});
+    }
+    
+        setSlider() {
+            this.img.setAttribute("src", this.slider[this.currentSlide].image);
+            this.title.innerHTML = this.slider[this.currentSlide].title;
+            this.text.innerHTML = this.slider[this.currentSlide].text;
+            this.link.innerHTML = this.slider[this.currentSlide].link;
+        }
+        
+        forward() {
+            this.currentSlide==2 ? (this.currentSlide = 0) : this.currentSlide++;
+            this.setSlider();
+        }
+        backward() {
+            this.currentSlide==0 ? (this.currentSlide = 2) : this.currentSlide--;
+            this.setSlider();
+        }
+
+        showMenu() {
+            this.isMenuOpened = true;
+            document.getElementById('logo').style.display = 'none';
+            document.getElementById('menuItems').style.display = 'flex';
+            document.getElementById('menuItems').style.fontWeight = 'bold';
+            document.getElementById('menu').style.backgroundColor = 'white';
+            document.getElementById('layerForSmallScreen').style.display = 'initial';
+            document.querySelectorAll('#menuItems li a').forEach(e => e.style.color = 'black');
+            document.getElementById('burger').classList.add('open');
+        }
+        hideMenu() {
+            this.isMenuOpened = false;
+            document.getElementById('logo').style.display = 'initial';
+            document.getElementById('menuItems').style.display = 'none';
+            document.getElementById('menuItems').style.fontWeight = 'initial';
+            document.getElementById('menu').style.backgroundColor = 'transparent';
+            document.getElementById('layerForSmallScreen').style.display = 'none';
+            document.querySelectorAll('#menuItems li a').forEach(e => e.style.color = 'white');
+            document.getElementById('burger').classList.remove('open');
+        }
+        
+    }
+
+    let newMain = new Main();
